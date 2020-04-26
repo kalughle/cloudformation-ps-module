0c9953785282a2ad8c760c96fdfbeec5f33eb4fb
Joe Waddington <Joe@Wolf.local>
2020-04-25 17:37:20 -0700

    events check

---
+++ powershell/getEvents.ps1
@@ -0,0 +1,9 @@
+﻿$a = Get-CFNStackEvent -StackName "cf-usw2-test01-vpc01"
+Start-Sleep -Seconds 5
+$b = Get-CFNStackEvent -StackName "cf-usw2-test01-vpc01"
+Start-Sleep -Seconds 10
+$c = Get-CFNStackEvent -StackName "cf-usw2-test01-vpc01"
+Start-Sleep -Seconds 20
+$d = Get-CFNStackEvent -StackName "cf-usw2-test01-vpc01"
+Start-Sleep -Seconds 30
+$e = Get-CFNStackEvent -StackName "cf-usw2-test01-vpc01"
